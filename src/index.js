31c4d9de6e21ee42aa63889c2350cb41cd5e738c
Ahmad Ragab <ahmadragab@protonmail.com>
2020-05-04 01:57:50 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+            }
+       );
+    }
+
+
+    render() {
+        return <div>latitude: {this.state.lat} </div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
