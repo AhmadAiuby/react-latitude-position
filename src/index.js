49db3711ecf3fbab6df9612bd6a3c73042de431a
Ahmad Ragab <ahmadragab@protonmail.com>
2020-05-09 02:48:30 +0200

    Avoiding Conditionals in Render

--- src/index.js
+++ src/index.js
@@ -14,19 +14,27 @@ componentDidMount(){
         err => this.setState({errorMessage: err.message})
     );
 }
-    render() {
-        
-            if(this.state.errorMessage && !this.state.lat){
-                return <div>Error: {this.state.errorMessage}</div>;
-            }
-           
-            if(!this.state.errorMessage && this.state.lat){
-                return <SeasonDisplay lat={this.state.lat} />
-            }
 
-            return <Spinner message="Please Accept location request" />;
-        
+renderContent() {
+    if(this.state.errorMessage && !this.state.lat){
+        return <div>Error: {this.state.errorMessage}</div>;
     }
+   
+    if (!this.state.errorMessage && this.state.lat){
+        return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please Accept location request" />;
+
+}
+
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );      
+    }            
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
